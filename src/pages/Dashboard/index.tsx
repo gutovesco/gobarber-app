90f9267a980534f7bddcb5d55defd764660b1b13
gutovesco <guto.vesco@hotmail.com>
2020-06-18 16:50:25 -0300

    routes and dashboard header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View, Button } from 'react-native';
-
+import {Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar} from './styles'
 import { useAuth } from '../../hooks/auth';
+import { useNavigation } from '@react-navigation/native';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const {navigate} = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate])
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem-vindo, {"\n"}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{uri: user.avatar_url}}></UserAvatar>
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
